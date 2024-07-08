643d19e4f89921f501ea9c48b9296ed84a00ea5c
charlesrobison <crobison@uw.edu>
2024-07-08 17:58:53 -0400

    correct naming error

--- models/customers.sql
+++ models/customers.sql
@@ -1,7 +1,7 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
     from {{ ref('stg_customers') }}
@@ -11,8 +11,8 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
     from {{ ref('stg_orders' )}}
